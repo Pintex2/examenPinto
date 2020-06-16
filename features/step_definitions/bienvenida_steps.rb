83c1e9f00917a64a149aafa089547fbac73b1b0f
Pintex2 <Andreumark@hotmail.com>
2020-06-16 09:42:35 -0400

    first feature passed

--- features/step_definitions/bienvenida_steps.rb
+++ features/step_definitions/bienvenida_steps.rb
@@ -2,6 +2,6 @@ Given('cuando estoy en la pagina principal') do
     visit '/'
   end
   
-  Then('deberia ver un mensaje {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  Then('deberia ver un mensaje {string}') do |message|
+    last_response.body.should =~ /#{message}/m
   end
